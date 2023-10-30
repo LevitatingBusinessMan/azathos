9918b1d0a6bca2c2b384c1aa7d4b717a511b8033
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2023-10-30 23:08:38 +0100

    my own init

--- Makefile
+++ Makefile
@@ -1,16 +1,20 @@
-cpio: directory #install
-	cd rootfs; find | cpio --quiet -o -H newc | gzip > ../rootfs.cpio.gz
+cpio: install
+	cd rootfs; find | cpio --quiet -o -H newc | gzip -1 > ../rootfs.cpio.gz
 
+install: build directory
+	cp target/x86_64-unknown-linux-musl/debug/init rootfs
+
+# Make a copy of the root/
 directory:
 	rm -rf rootfs
 	mkdir rootfs
 	cp -r root/* rootfs
 
-install:
-	mv target/debug/init rootfs
+build:
+	cargo build
 
 run:
-	qemu-system-x86_64 -kernel /boot/vmlinuz-linux -append "console=ttyS0" -initrd rootfs.cpio.gz -nographic -m 256
+	qemu-system-x86_64 -kernel /boot/vmlinuz-linux -append "console=ttyS0 init=/init loglevel=7" -initrd rootfs.cpio.gz -nographic -m 1024
 
 clean:
 	rm rootfs.cpio.gz
--- init/src/main.rs
+++ init/src/main.rs
@@ -1,5 +1,79 @@
+#![feature(panic_backtrace_config)]
+use std::{os::unix::process::CommandExt, ffi::CString};
+use anyhow::*;
+use libc::c_char;
+use serde::Deserialize;
+
+static CONFIG_FILE: &'static str = "/etc/init.toml";
+
+#[derive(Deserialize)]
+struct Config {
+    shell: String,
+    mounts: Vec<Mount>,
+}
+
+#[derive(Deserialize)]
+struct Mount {
+    src: String,
+    dst: String,
+    #[serde(alias = "type")]
+    type_: String,
+    flags: String,
+}
+
+impl Mount {
+    pub unsafe fn mount(&self) -> Result<i32> {
+        Ok(libc::mount(
+            CString::new(self.src.clone())?.as_ptr(),
+            CString::new(self.dst.clone())?.as_ptr(),
+            CString::new(self.type_.clone())?.as_ptr(),
+            0, std::ptr::null())
+        )
+    }
+}
+
+macro_rules! log {
+    ($msg:expr, $expr:expr) => {{
+        print!("{}... ", $msg);
+        let result = $expr;
+        println!("done");
+        result
+    }};
+}
 
 fn main() {
-    println!("Hello, world!");
-    loop {}
+    println!("Init started");
+
+    match init() {
+        Result::Ok(_) => loop { std::thread::park() },
+        Result::Err(e) => {
+            println!("{e}");
+            loop { std::thread::park() }
+        }
+    }
+}
+
+fn init() -> Result<()> {
+    let config: Config = log!("Reading config file", {
+        let config_file = std::fs::read_to_string(CONFIG_FILE)?;
+        toml::from_str(&config_file)?
+    });
+
+    mounts(config.mounts)?;
+
+    println!("Forking off a shell. Stay safe!");
+    std::process::Command::new(config.shell).spawn()?;
+    Ok(())
+}
+
+fn mounts(mounts: Vec<Mount>) -> Result<()> {
+    for mount in mounts {
+        log!(format!("Mounting {}", mount.src), unsafe {
+            let r = mount.mount()?;
+            if r != 0 {
+                bail!("{}", std::io::Error::last_os_error());
+            };
+        });
+    }
+    Ok(())
 }
