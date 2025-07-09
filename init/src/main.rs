57e2a5fd01ee8bc8c3605d68ec29544beaec1650
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2025-07-09 06:55:56 +0200

    improve error handling in init

--- init/src/main.rs
+++ init/src/main.rs
@@ -1,8 +1,9 @@
 #![feature(panic_backtrace_config)]
-use std::{os::unix::{process::CommandExt, prelude::MetadataExt}, ffi::CString};
+#![feature(let_chains)]
+use std::{ffi::CString, fmt::Display, os::unix::{prelude::MetadataExt, process::CommandExt}, path::{Path, PathBuf}};
 use anyhow::*;
 use serde::Deserialize;
-use color::green;
+use color::{green, red};
 
 static CONFIG_FILE: &'static str = "/etc/init.toml";
 
@@ -30,13 +31,17 @@ struct Mount {
 }
 
 impl Mount {
-    pub unsafe fn mount(&self) -> Result<i32> {
-        Ok(libc::mount(
+    pub unsafe fn mount(&self) -> std::result::Result<(), std::io::Error> {
+        if libc::mount(
             CString::new(self.src.clone())?.as_ptr(),
             CString::new(self.dst.clone())?.as_ptr(),
             CString::new(self.type_.clone())?.as_ptr(),
             0, std::ptr::null())
-        )
+        < 0 {
+            Err(std::io::Error::last_os_error())
+        } else {
+            std::io::Result::Ok(())
+        }
     }
 }
 
@@ -44,7 +49,11 @@ macro_rules! log {
     ($msg:expr, $expr:expr) => {{
         print!("{}... ", $msg);
         let result = $expr;
-        println!("{}", green!("done"));
+        if result.is_ok() {
+            println!("{}", green!("done"))
+        } else {
+             println!("{}", red!("failed"))
+        }
         result
     }};
 }
@@ -66,10 +75,10 @@ fn main() {
 fn init() -> Result<()> {
     let config: Config = log!("Reading config file", {
         let config_file = std::fs::read_to_string(CONFIG_FILE)?;
-        toml::from_str(&config_file)?
-    });
+        toml::from_str::<Config>(&config_file)
+    })?;
 
-    mounts(config.mounts)?;
+    mounts(config.mounts);
 
     println!("Forking off a shell. Stay safe!");
     std::process::Command::new(&config.login.shell)
@@ -82,14 +91,10 @@ fn init() -> Result<()> {
     Ok(())
 }
 
-fn mounts(mounts: Vec<Mount>) -> Result<()> {
+fn mounts(mounts: Vec<Mount>) {
     for mount in mounts {
-        log!(format!("Mounting {}", mount.src), unsafe {
-            let r = mount.mount()?;
-            if r != 0 {
-                bail!("{}", std::io::Error::last_os_error());
-            };
-        });
+        if let Err(e) = log!(format!("Mounting {}", mount.src), unsafe { mount.mount() }) {
+            println!("{}", e);
+        }
     }
-    Ok(())
 }
